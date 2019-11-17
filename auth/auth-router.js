c8def186fdffc777cce66f9d395f40a1102cd0e6
umekow <umekolashun@gmail.com>
2019-11-17 12:38:25 -0600

    authentication

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,71 @@
 const router = require('express').Router(); 
 
-const bycrpty = require('bcryptjs'); 
+const bcrypt = require('bcryptjs'); 
 const jwt = require('jsonwebtoken'); 
 
+const Users = require('../users/user-model.js'); 
+const {validateUser} = require('../users/users-helper.js');
+
+
+router.post('/register', (req, res) => {
+    let user = req.body; 
+
+    const validatedUser = validateUser(user) 
+    if(validatedUser.isSuccessful === true){
+        const hash = bcrypt.hashSync(user.password, 10); 
+        user.password = hash; 
+
+        Users
+            .add(user)
+            .then(r => {
+                res.status(201).json(r)
+            })
+            .catch(e => {
+                res.status(500).json(e)
+            })
+
+    }else {
+        res.status(400).json({
+            message: "Invalid input", 
+            errors: validatedUser.errors
+        })
+    }
+})
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body; 
+
+    Users.find({username})
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)){
+            const token = getJwtToken(user.username); 
+      
+            res.status(200).json({
+              message: `Welcome ${user.username}!`, 
+              token
+            })
+          } else {
+            res.status(401).json({message: "Invalid Credentials"})
+          }
+        })
+        .catch(error => {
+          res.status(500).json(error)
+        })
+});
+
+const getJwtToken = username => {
+    const payload = {
+        username
+    }
+
+    const secret = process.env.JWT_SECRET || "secret";
+
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, secret, options)
+}
+
+module.exports = router; 
--- organizations/org-router.js
+++ organizations/org-router.js
@@ -5,8 +5,9 @@ const Org = require('./org-model.js');
 
 router.get('/', (req, res) => {
     Org.findByName(req.body.name)
-    .then(r => res.status(200).json(r))
-    .catch(e=> console.log(e))
+        .first()
+        .then(r => res.status(200).json(r))
+        .catch(e=> console.log(e))
 })
 
 module.exports = router
