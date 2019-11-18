c8c1bd768be3927d017e0a5e170d59b4b9068351
umekow <umekolashun@gmail.com>
2019-11-17 19:07:57 -0600

    forgot an endpoint

--- donations/donations-model.js
+++ donations/donations-model.js
@@ -7,11 +7,15 @@ const addDonation = donation => knex('donations').insert(donation)
 const updateDonation = (id, changes) => knex('donations').where({id}).update({...changes})
 
 //get donation by id 
-const getDonationById = id => knex('donations').where({id}).first()
+const getDonationById = id => knex('donations').where({id}).first(); 
 
 //delete donation
-const deleteDonation = id => knex('donations').where({id}).delete()
+const deleteDonation = id => knex('donations').where({id}).delete();
 
+//get all donations to a campaign 
+const donationsTo = campagin_id => knex('donations').where({campagin_id}); 
 
+//get all donations made by a donor 
+const donationsBy = donor_id => knex('donations').where({donor_id}); 
 
-module.exports = {addDonation, updateDonation, getDonationById, deleteDonation}
+module.exports = {addDonation, updateDonation, getDonationById, deleteDonation, donationsTo, donationsBy}
