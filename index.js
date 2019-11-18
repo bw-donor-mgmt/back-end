f695bf7446d396b15ecb85f69c19233bf498a11f
umekow <umekolashun@gmail.com>
2019-11-18 12:58:16 -0600

    forgot secret

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //import server
 const server = require('./server.js'); 
 
+//authentication
+const auth = require('./auth/auth-middleware.js');
 //routers
 const OrgRouter = require('./organizations/org-router.js');
 const AuthRouter = require('./auth/auth-router.js');
@@ -11,11 +13,11 @@ const DonationsRouter = require('./donations/donations-router.js');
 
 //base endpoints for different categories
 server.use('/auth', AuthRouter); 
-server.use('/user', UsersRouter); 
-server.use('/organizations', OrgRouter); 
-server.use('/campaigns', CampaignsRouter); 
-server.use('/donors', DonorsRouter); 
-server.use('/donations', DonationsRouter); 
+server.use('/user', auth,  UsersRouter); 
+server.use('/organizations', auth, OrgRouter); 
+server.use('/campaigns', auth, CampaignsRouter); 
+server.use('/donors', auth, DonorsRouter); 
+server.use('/donations',auth,  DonationsRouter); 
 
 
 server.get('/', (req, res) => res.status(200).json({message: 'Server is running'})); 
