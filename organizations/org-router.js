9f91a4e932b13699445741541c2c469503fbecc8
umekow <umekolashun@gmail.com>
2019-11-17 20:30:22 -0600

    organization router

--- organizations/org-router.js
+++ organizations/org-router.js
@@ -2,12 +2,37 @@ const router = require('express').Router();
 
 const Org = require('./org-model.js'); 
 
-
-router.get('/', (req, res) => {
-    Org.findByName(req.body.name)
+//get organization by name
+router.get('/:name', (req, res) => {
+    Org.findByName(req.params.name)
         .first()
         .then(r => res.status(200).json(r))
-        .catch(e=> console.log(e))
+        .catch(e => res.status(400).json(e))
+})
+
+//get organization by email 
+router.get('/:id', (req, res) => {
+    Org
+        .findOrgById(req.params.id)
+        .first()
+        .then(r => res.status(200).json(r))
+        .catch(e => res.status(400).json(e))
+})
+
+//update organization
+router.put('/:id', (req, res) => {
+    Org
+        .updateOrg(req.params.id)
+        .then(r => res.status(202).json(r))
+        .catch(e => res.status(400).json(e))
+})
+
+//delete organization
+router.delete('/:id', (req, res) => {
+    Org
+        .deleteOrg(req.params.id)
+        .then(r => res.status(202).json(r))
+        .catch(e => res.status(400).json(e))
 })
 
 module.exports = router
