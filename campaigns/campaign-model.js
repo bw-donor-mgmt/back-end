e907939e0c6af7828de0790770d88f57be1723c2
umekow <umekolashun@gmail.com>
2019-11-17 19:23:10 -0600

    campaign model

--- campaigns/campaign-model.js
+++ campaigns/campaign-model.js
@@ -1,13 +1,18 @@
 const knex = require('../data/db-config.js'); 
 
 //create campaign
+const addCampaign = campaign => knex('campaigns').insert(campaign); 
 
 //get all campaigns for organization
+const getCampaignsByOrg = organization_id => knex('campaigns').where({organization_id});
 
 //get campaign by id
+const getCampaignById = id => knex('campaigns').where({id});
 
 //update campaign
+const updateCampaign = id => knex('campaigns').where({id}).update()
 
 //delete campaign
+const deleteCampaign = id => knex('campaigns').where({id}).delete();
 
-module.exports = {}
+module.exports = {addCampaign, getCampaignsByOrg, getCampaignById, updateCampaign, deleteCampaign}
