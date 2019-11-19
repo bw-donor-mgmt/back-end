2b561ad65a4636439a2734960e6e2e8cd6716f2b
umekow <umekolashun@gmail.com>
2019-11-19 17:48:21 -0600

    campaign seeds

--- data/seeds/001-campaigns.js
+++ data/seeds/001-campaigns.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, name: 'Buy Supplies for Downtown Clinic', description: 'The Downtown Clinic provides healthcare to thousands of citizens each year. The clinic needs your help to expand their services by buying more supplies including medicine and beds. ', goal: 50000, raised: 0 , organization_id: 1},
+        {id: 2, name: 'Clean Downtown Jackson', description: 'Please help us clean up our city so visitors can know that Jackson is the best city in the state', goal: 1000, raised: 0 , organization_id: 2},
+        {id: 3, name: 'Introduce Murrah Middle School Students to JRR', description: 'Help us supply the library of Murrah Junior High with JRR Tolkien books so they can develop a new passion for dwarves, elves, and hobbits! ', goal: 5000, raised: 0 , organization_id: 3}
       ]);
     });
 };
