30439ac5fd3697083a112cf56245f1570017a65c
umekow <umekolashun@gmail.com>
2019-11-20 14:23:24 -0600

    .

--- donors/donors-model.js
+++ donors/donors-model.js
@@ -3,13 +3,13 @@ const knex = require('../data/db-config.js');
 //all donors
 const getDonors = () => knex('donors')
 //add a donor
-const addDonor = donor => knex('donors').insert(donor).returning('id', 'name', 'email', 'contacted_on', 'method'); 
+const addDonor = donor => knex('donors').returning('id', 'name', 'email', 'contacted_on', 'method').insert(donor);
 
 //get donor by id
 const findDonorById = id => knex('donors').where({id});
 
 //update donor
-const updateDonor = (id, changes) => knex('donors').where('id', id).update(changes);
+const updateDonor = (id, changes) => knex('donors').where('id', id).update(changes).returning();
 
 //delete donor
 const deleteDonor = id => knex('donors').where({id}); 
