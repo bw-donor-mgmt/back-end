1a845da949b9607602ea90f2a5c94d856628804e
umekow <umekolashun@gmail.com>
2019-11-19 20:56:25 -0600

    replaced insert with truncate

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,10 +1,11 @@
 
 exports.seed = function(knex) {
       // Inserts seed entries
-      return knex('users').insert([
+      return knex('users').truncate().then(() => {
+        insert([
         {username: 'user1', password: 'pass1', organization: '1'},
         {username: 'user2', password: 'pass2', organization: '2'},
         {username: 'user3', password: 'pass3', organization: '1'}
       ]);
-  
+    })
 };
