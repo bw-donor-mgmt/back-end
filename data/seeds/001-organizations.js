7bc7bd217e556b32169d5e8c7c603f0e15c01d94
umekow <umekolashun@gmail.com>
2019-11-20 07:59:18 -0600

    fixed config file

--- data/seeds/001-organizations.js
+++ data/seeds/001-organizations.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('organizations').truncate().then(() => {
         return knex('organizations').insert([
-        {name: 'Health 4 Jackson', mission: 'To provide health care to those who cannot afford it!'},
-        {name: 'Cleaner Jackson', mission: 'To help clean the streets of Jackson'},
-        {name: 'JRR Fans Unit', mission: 'To provide a place where J.R.R Tolkein fans can meet and have fun. '}
+        {id: 1, name: 'Health 4 Jackson', mission: 'To provide health care to those who cannot afford it!'},
+        {id: 2, name: 'Cleaner Jackson', mission: 'To help clean the streets of Jackson'},
+        {id: 3, name: 'JRR Fans Unit', mission: 'To provide a place where J.R.R Tolkein fans can meet and have fun. '}
       ]);
     });
 };
