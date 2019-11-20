44b09dc132bfef41023d394106ce624a14e24352
umekow <umekolashun@gmail.com>
2019-11-20 09:00:06 -0600

    bcrypt pass

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -1,11 +1,12 @@
+const bcrypt = require('bcryptjs'); 
 
 exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').del().then(() => {
         return knex('users').insert([
-        {username: 'user1', password: 'pass1', organization_id: '1'},
-        {username: 'user2', password: 'pass2', organization_id: '2'},
-        {username: 'user3', password: 'pass3', organization_id: '1'}
+        {username: 'user1', password: bcrypt.hashSync('pass1'), organization_id: '1'},
+        {username: 'user2', password: bcrypt.hashSync('pass2'), organization_id: '2'},
+        {username: 'user3', password: bcrypt.hashSync('pass3'), organization_id: '1'}
       ]);
     })
 };
